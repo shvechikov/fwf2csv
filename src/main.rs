a8148e2075a75c166accc2d51aa76fdbf85f6fd6
Leonid Shvechikov <shvechikov@gmail.com>
2020-04-24 11:10:21 -0700

    pass fsf config as a command line argument

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,10 @@
-use std::env;
-use std::fs::File;
-use std::io::{self, BufReader, BufWriter, Write};
-use std::path::Path;
-
-use bstr::{io::BufReadExt, ByteSlice};
-use csv;
+use std::process;
 
 mod lib;
 
-fn main() -> std::io::Result<()> {
-    let mut reader = BufReader::new(io::stdin());
-    let mut writer = csv::Writer::from_writer(io::stdout());
-    lib::parse_gdmi(&mut reader, &mut writer)?;
-    Ok(())
+fn main() {
+    if let Err(err) = lib::parse_gdmi() {
+        println!("{}", err);
+        process::exit(1);
+    }
 }
