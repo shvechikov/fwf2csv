75824dbbdf501de05f55cd6f0c750e8b8310219b
Leonid Shvechikov <shvechikov@gmail.com>
2020-04-25 00:16:52 -0700

    rename config -> schema

--- src/lib.rs
+++ src/lib.rs
@@ -7,25 +7,25 @@ use csv;
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
-struct ConfigRecord {
+struct SchemaRecord {
     column: String,
     start: usize,
     length: usize,
 }
 
-fn read_config(schema_file: File) -> Result<Vec<ConfigRecord>, Box<dyn Error>> {
+fn read_schema(schema_file: File) -> Result<Vec<SchemaRecord>, Box<dyn Error>> {
     let mut csv_reader = csv::Reader::from_reader(schema_file);
-    let mut config: Vec<ConfigRecord> = Vec::new();
+    let mut schema: Vec<SchemaRecord> = Vec::new();
 
     for result in csv_reader.deserialize() {
-        let record: ConfigRecord = result?;
-        config.push(record);
+        let record: SchemaRecord = result?;
+        schema.push(record);
     }
-    Ok(config)
+    Ok(schema)
 }
 
-fn parse_line<'a>(line: &'a [u8], config: &'a Vec<ConfigRecord>) -> impl Iterator<Item = &'a [u8]> {
-    config
+fn parse_line<'a>(line: &'a [u8], schema: &'a Vec<SchemaRecord>) -> impl Iterator<Item = &'a [u8]> {
+    schema
         .iter()
         .map(move |f| line[f.start..(f.start + f.length)].trim())
 }
@@ -33,10 +33,10 @@ fn parse_line<'a>(line: &'a [u8], config: &'a Vec<ConfigRecord>) -> impl Iterato
 pub fn parse_gdmi(schema_file: File) -> io::Result<()> {
     let reader = BufReader::new(io::stdin());
     let mut writer = csv::Writer::from_writer(io::stdout());
-    let config = read_config(schema_file).unwrap();
+    let schema = read_schema(schema_file).unwrap();
 
     reader.for_byte_line(|line| {
-        let lazy_record = parse_line(&line, &config);
+        let lazy_record = parse_line(&line, &schema);
         writer.write_record(lazy_record)?;
         writer.flush()?;
         Ok(true)
