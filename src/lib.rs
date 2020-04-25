d9340271d0bee47f82d5d17bea1168ddeed32525
Leonid Shvechikov <shvechikov@gmail.com>
2020-04-25 00:08:09 -0700

    add --help and better argument parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,8 @@
-use std::env;
 use std::error::Error;
 use std::fs::File;
 use std::io::{self, BufReader};
-use std::path::Path;
 
-use bstr::{ByteSlice, io::BufReadExt};
+use bstr::{io::BufReadExt, ByteSlice};
 use csv;
 use serde::Deserialize;
 
@@ -15,22 +13,8 @@ struct ConfigRecord {
     length: usize,
 }
 
-fn get_config_file() -> File {
-    let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
-    let path = Path::new(filename);
-    let display = path.display();
-
-    let input_file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", display, why),
-        Ok(input_file) => input_file,
-    };
-    input_file
-}
-
-fn read_config() -> Result<Vec<ConfigRecord>, Box<dyn Error>> {
-    let config_file = get_config_file();
-    let mut csv_reader = csv::Reader::from_reader(config_file);
+fn read_config(schema_file: File) -> Result<Vec<ConfigRecord>, Box<dyn Error>> {
+    let mut csv_reader = csv::Reader::from_reader(schema_file);
     let mut config: Vec<ConfigRecord> = Vec::new();
 
     for result in csv_reader.deserialize() {
@@ -40,17 +24,16 @@ fn read_config() -> Result<Vec<ConfigRecord>, Box<dyn Error>> {
     Ok(config)
 }
 
-
 fn parse_line<'a>(line: &'a [u8], config: &'a Vec<ConfigRecord>) -> impl Iterator<Item = &'a [u8]> {
     config
         .iter()
         .map(move |f| line[f.start..(f.start + f.length)].trim_end())
 }
 
-pub fn parse_gdmi() -> io::Result<()> {
+pub fn parse_gdmi(schema_file: File) -> io::Result<()> {
     let reader = BufReader::new(io::stdin());
     let mut writer = csv::Writer::from_writer(io::stdout());
-    let config = read_config().unwrap();
+    let config = read_config(schema_file).unwrap();
 
     reader.for_byte_line(|line| {
         let lazy_record = parse_line(&line, &config);
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,43 @@
+use clap::{App, Arg};
+use std::fs::File;
+use std::path::Path;
 use std::process;
-
 mod lib;
 
+const AFTER_HELP: &'static str = "\
+EXAMPLE:
+    cat input_file.fwf | fwf2csv input_schema.csv > output.csv
+
+SCHEMA FILE EXAMPLE:
+    > cat input_schema.csv
+    column,start,length
+    field_1,0,9
+    field_2,9,5
+    field_3,14,4
+";
+
 fn main() {
-    if let Err(err) = lib::parse_gdmi() {
+    let matches = App::new("fwf2csv")
+        .version("0.1.0")
+        .about("Converts fixed-width files (FWF) to comma separated (CSV).")
+        .after_help(AFTER_HELP)
+        .arg(
+            Arg::with_name("schema")
+                .required(true)
+                .help("Fixed-width schema file")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let filename = matches.value_of("schema").unwrap();
+    let path = Path::new(filename);
+    let display = path.display();
+    let schema_file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(schema_file) => schema_file,
+    };
+
+    if let Err(err) = lib::parse_gdmi(schema_file) {
         println!("{}", err);
         process::exit(1);
     }
